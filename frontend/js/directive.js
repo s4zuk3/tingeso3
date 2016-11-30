78d58dbba276e1d4d81479f431725dc9a431ba82
Francisco <francisco.lopez.a@usach.cl>
2016-11-30 19:04:32 -0300

    dsa

---
+++ frontend/js/directive.js
@@ -0,0 +1,14 @@
+// Directiva que convierte a integer un numero en el campo "value" de la etiqueta select option html.
+TingesoApp.directive('convertToNumber', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(val) {
+        return parseInt(val, 10);
+      });
+      ngModel.$formatters.push(function(val) {
+        return '' + val;
+      });
+    }
+  };
+});
