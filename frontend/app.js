1c9f8dfa31bd353c0691e7191226d1d1a3aa7833
Francisco <francisco.lopez.a@usach.cl>
2016-11-30 17:18:50 -0300

    Mostrar producto v0.9

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,11 @@
 var tingesoApp = angular.module('tingesoApp', [
 	"ngRoute",
 	"satellizer",
-	"toastr"
-	]);
-
+	"toastr",
+	"ngMaterial",
+	"ngAnimate",
+	"ui.bootstrap"
+	])
 
 var URL_API = 'https://localhost:8181/backend';
 // No agregar nada a este archivo. (Solo dependencias)
