e67a19e64fb36a3a0d743afe8413eaf5eb173c2f
Francisco <francisco.lopez.a@usach.cl>
2016-11-30 17:37:17 -0300

    sdasd

--- frontend/js/controller/home.js
+++ frontend/js/controller/home.js
@@ -1,5 +1,6 @@
 tingesoApp.controller('HomeController',function($scope,$location,Productos_service){
 	$scope.productos = {};
+	$scope.testing = [{nombre:'hola'},{nombre:'chao'}];
 	Productos_service.getAllProductos().then(function(response) {
         	$scope.productos = response.data;
         	alert(JSON.stringify($scope.productos));
