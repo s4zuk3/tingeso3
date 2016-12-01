4373306fd8439e9cb4adf1b8600d6d5213966cc9
Francisco <francisco.lopez.a@usach.cl>
2016-11-30 23:30:45 -0300

    asdf

--- frontend/js/controller/productoForm.js
+++ frontend/js/controller/productoForm.js
@@ -1,17 +1,19 @@
 tingesoApp.controller('ProductoFormController',function($scope,$rootScope,$location,Productos_service){
 	$scope.newProducto = {
-		'fotoProducto':'',
+		'fotoProducto':'test.jpg',
 		'idProducto': $rootScope.idProducto+1
 	};
-
-	$scope.submit = function(newProducto,$flow){
+	alert(Flow.FlowFile);
+	alert(JSON.stringify(Flow.FlowFile));
+	alert(Flow.FlowFile.name);
+	$scope.submit = function(newProducto){
 		//Funciones de validaciones
 		// Si hay algun error se cambian los scope.error y return;
 
 		//Logica variada
-		alert($flow);
-		newProducto.fotoProducto = $flow.files[0];
-		alert(newProducto);
+
+		//newProducto.fotoProducto = $flow.files[0];
+
 		Productos_service.createProducto(newProducto).then(function(response) {
 
 			if(response.status == 200) // creacion OK
