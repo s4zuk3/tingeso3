06f00521c9a807b196df1fbfa68ed3fe98efd8da
Francisco <francisco.lopez.a@usach.cl>
2016-11-30 22:14:27 -0300

    asfd

--- backend/src/main/java/service/ProductoService.java
+++ backend/src/main/java/service/ProductoService.java
@@ -10,6 +10,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import facade.ProductoFacade;
 import model.Producto;
