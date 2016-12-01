a1eaf8081916e70690da08a2bad61e8dc8fa23ca
Francisco <francisco.lopez.a@usach.cl>
2016-11-30 21:52:26 -0300

    casi

--- frontend/js/controller/home.js
+++ frontend/js/controller/home.js
@@ -1,24 +1,14 @@
 tingesoApp.controller('HomeController',function($scope,$rootScope,$location,Productos_service){
 	$scope.productos = {};
-	
 	Productos_service.getAllProductos().then(function(response) {
 		    $rootScope.idProducto = 0;
         	$scope.productos = response.data;
    			var data = response.data;
-   			alert(JSON.stringify(data));
 			for (var i = 0; i < data.length; i++){
     			 var pro = data[i];
-    			 alert(JSON.stringify(pro));
-   				alert(pro.idProducto + " "+ $rootScope.idProducto);
    				if(pro.idProducto > $rootScope.idProducto){
    					$rootScope.idProducto = pro.idProducto;
-   					alert($rootScope.idProducto);
    				}
 			}
-
-
-
-   			
         });
-
 });
