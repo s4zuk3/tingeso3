e6e4d8a5ff7a17b49dc24cdce21476603fa46ed6
Francisco Lopez <francisco.lopez.a@usach.cl>
2016-12-04 21:37:26 -0300

    gradle check

---
+++ frontend/mantis/config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantisbt_db';
+$g_db_username            = 'mantis';
+$g_db_password            = 'tingeso123';
+
+$g_default_timezone       = 'America/Santiago';
+
+$g_crypto_master_salt     = 'Qe5r7LrSHz/pdOXYWwGG0uvXT0Gs5vXgPZGPSyRsff0=';
